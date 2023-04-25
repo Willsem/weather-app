4eba184a884fb11baea435ff922f1881485308b8
a.o.stepanov <a.o.stepanov@tinkoff.ru>
2023-04-25 21:35:37 +0300

    install MUI

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import suidPlugin from '@suid/vite-plugin';
 import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-  plugins: [solidPlugin(), eslint()],
+  plugins: [solidPlugin(), suidPlugin(), eslint()],
   server: {
     port: 3000,
   },
